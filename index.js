d3844fb42ec75e2049ad84c44e641000e012bdf7
LaneMiller <chris.lane.miller@gmail.com>
2018-02-07 02:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(array) {
   var facts = [];
   
-  let i = 0;
-  while (i < array.length) {
+  for (let i = 0; i < array.length; i++) {
     facts.push(`${array[i]}!!!`);
-    console.log(i++);
-  }
+  } 
   
+  return (facts);
+}
+
 function iLoveTheBeatles(n) {
   var groupie = [];
   do {
